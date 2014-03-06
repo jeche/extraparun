9fd9e2ce48b12304d7fa4fe74640f06ef4fcdc2b
Jessica Chen <jlchen@email.wm.edu>
2014-03-05 23:30:23 -0500

    Added password form
    password form added along with update of signup page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Extraparun::Application.routes.draw do
   resources :users
 
   root "users#index"
+  match '/signup', to: 'users#new', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
