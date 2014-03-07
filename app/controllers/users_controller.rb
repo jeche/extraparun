6467807d7ec382064f17e18286de5cb89ed3b351
Jessica Chen <jlchen@email.wm.edu>
2014-03-06 22:32:08 -0500

    sign in and sign out working fixes #7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
       else
         format.html { render action: 'new' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -69,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
