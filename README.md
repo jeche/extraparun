6871a9486df6bae689c0b1de9d92166b6741f082
Jessica Chen <jlchen@email.wm.edu>
2014-03-07 00:13:11 -0500

    Fix:#1
    First rails app is technically generated

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 extraparun
 ==========
+<<<<<<< HEAD
+CURRENT PHASE: ADDING WORK ITEMS
+================================
+Currently adding Rails Hello World app and links to read on how to build a rails application on your computer.  Goal for spring break: Upload a version that has all the database connections mapped.
+
+Tutorials for Ruby on Rails: http://guides.rubyonrails.org/getting_started.html
+=======
 CURRENT PHASE: RESEARCH/DESGIN
 ==========
 The goals of the project
@@ -23,3 +30,4 @@ Technical Details
   - Python or Java
   - Scripts for building database
 
+>>>>>>> parent of ded32d2... Update README.md
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Extraparun::Application.config.secret_key_base = '705929e1f6f051bfb358828ee1e4a992e30e689b8de2a11b73aa39ca975ab1bee7e149de933b783e500f85311dc7cb2e42db7764fb9b3fbca1851d37ba327fb9'
