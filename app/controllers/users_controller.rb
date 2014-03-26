87bfb438b35039d07fb8a25cd3683dcc6b69ea8f
jeche <jlchen@email.wm.edu>
2014-03-26 11:48:18 -0400

    added some updates to force user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+
 
   # GET /users
   # GET /users.json
@@ -71,5 +74,14 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
