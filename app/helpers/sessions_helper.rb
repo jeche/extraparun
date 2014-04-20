1bf8c98dcad5019baab3cdce0711306bd3867b74
atjones <atjones@email.wm.edu>
2014-04-20 18:39:49 -0400

    Fixed security for all pages.  Should only be able to access your own pages.  closes #36

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,6 +16,12 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
+	def signed_in_user
+    	unless signed_in?
+      		redirect_to signinpage_url, notice: "Please sign in."
+    	end
+  	end
+
 	def current_user=(user)
 		@current_user = user
 	end
