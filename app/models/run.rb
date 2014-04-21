f350f84ee3afecb833f6f30ad7150085ca97488f
jeche <jlchen@email.wm.edu>
2014-04-21 00:49:15 -0400

    Finished the error checking for the runs page -- it's not pretty, but it works.  closes #61

--- app/models/run.rb
+++ app/models/run.rb
@@ -2,9 +2,10 @@ class Run < ActiveRecord::Base
 	has_many :courses, :dependent => :destroy, as: :runnable 
 	has_many :routes, :through => :courses
 	belongs_to :user
-	validates :name, presence: true
-	validates :date, presence: true
-	validates :hr, presence: true
-	validates :min, presence: true
-	validates :sec, presence: true
+	validates_presence_of :name, :date, :hr, :min, :sec
+	validates_numericality_of   :hr, :only_integer => true
+	validates_numericality_of   :min, :only_integer => true
+	validates_numericality_of   :sec, :only_integer => true
+	validates_numericality_of   :temp
+	validates_numericality_of   :humidity
 end
